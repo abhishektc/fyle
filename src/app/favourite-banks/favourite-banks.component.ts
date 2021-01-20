935af0f2fcc271520998ba199b7dd65391e1a02d
Abhishek T C <abhishekgowda4258@gmail.com>
2021-01-20 14:45:33 +0530

    Removing paginator from favourite page

--- src/app/favourite-banks/favourite-banks.component.ts
+++ src/app/favourite-banks/favourite-banks.component.ts
@@ -1,7 +1,6 @@
 import { BanksDetailsServiceService } from './../banks-details-service.service';
-import { Component, OnInit, ViewChild } from '@angular/core';
-import {MatPaginator} from '@angular/material/paginator';
-import {MatTableDataSource} from '@angular/material/table';
+import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-favourite-banks',
@@ -11,30 +10,26 @@ import {MatTableDataSource} from '@angular/material/table';
 export class FavouriteBanksComponent implements OnInit {
   displayedColumns: string[] = ['ifsc', 'bankName', 'city', 'branch', 'favourite', 'action'];
   dataSource;
-  favourites:any = [];
-  
-  constructor(private bankService:BanksDetailsServiceService) { }
+  favourites: any = [];
 
-  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+  constructor(private bankService: BanksDetailsServiceService) { }
 
   ngOnInit(): void {
     for (let i = 0; i < sessionStorage.length; i++) {
       this.favourites.push(this.bankService.getFavourite(sessionStorage.key(i)));
     }
-    this.dataSource =new MatTableDataSource(this.favourites)
-    this.dataSource.paginator = this.paginator;  
+    this.dataSource = new MatTableDataSource(this.favourites)
   }
 
-  deletefromFavourites(ifsc:any){
+  deletefromFavourites(ifsc: any) {
     sessionStorage.removeItem(ifsc);
     for (let i = 0; i < this.favourites.length; i++) {
-      if(ifsc === this.favourites[i].ifsc){
-        this.favourites.splice(i,1);
+      if (ifsc === this.favourites[i].ifsc) {
+        this.favourites.splice(i, 1);
       }
     }
     alert("Removed Successfully");
-    this.dataSource =new MatTableDataSource(this.favourites)
-    this.dataSource.paginator = this.paginator;  
+    this.dataSource = new MatTableDataSource(this.favourites)
   }
 
 }
